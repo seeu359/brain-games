bf146d9e509f721f50339b38d4af241b2d079190
seeu359 <a.ch3re359@yandex.ru>
2022-06-18 21:04:16 +0300

    Add new game: "Brain-prime"

---
+++ brain_games/games/brain_prime.py
@@ -0,0 +1,33 @@
+from brain_games.game_logic import *
+
+
+def brain_prime():
+    name_user = welcome_user()
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+    correct_answer = 'yes'
+    incorrect_answer = 'no'
+    counter_correct = 0
+    while counter_correct < 3:
+        list_for_number = []
+        random_number = randint(min_num, max_num)
+        print(f'Question: {random_number}')
+        for i in range(1, random_number + 1):
+            if random_number % i == 0:
+                list_for_number.append(i)
+        answer = prompt.string('Your answer: ')
+        if len(list_for_number) == 2 and (answer == correct_answer):
+            print('Correct')
+            counter_correct += 1
+        elif len(list_for_number) > 2 and (answer == incorrect_answer):
+            print('Correct')
+            counter_correct += 1
+        elif len(list_for_number) == 2 and (answer == incorrect_answer):
+            print(f'{answer} is wrong answer ;(. Correct answer was {correct_answer}.'
+                  f'\nLet\'s try again, {name_user}!')
+            break
+        elif len(list_for_number) > 2 and (answer == correct_answer):
+            print(f'{answer} is wrong answer ;(. Correct answer was {incorrect_answer}.'
+                  f'\nLet\'s try again, {name_user}!')
+            break
+    if counter_correct == 3:
+        congrats_win(name_user)
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from brain_games.game_logic import *
+from brain_games.games import brain_prime
+
+
+def main():
+    welcome()
+    brain_prime.brain_prime()
+
+
+if __name__ == "__main__":
+    main()
