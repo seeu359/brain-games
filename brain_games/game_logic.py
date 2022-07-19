4fd63af1c9bb78705f6341d1384da30b1781caf3
seeu359 <a.ch3re359@yandex.ru>
2022-07-19 23:46:03 +0300

    refactoring, 6

--- brain_games/game_logic.py
+++ brain_games/game_logic.py
@@ -1,6 +1,6 @@
 import prompt
 
-MAX_ROUNDS = 3
+ROUND_NUMBER = 3
 
 
 def welcome_user():
@@ -10,15 +10,11 @@ def welcome_user():
     return name
 
 
-def congrats_win(name):
-    print(f'Congratulations, {name}!')
-
-
 def launch_game_round(game):
     username = welcome_user()
     round_counter = 0
     print(game.GAME_QUESTION)
-    while round_counter < MAX_ROUNDS:
+    while round_counter < ROUND_NUMBER:
         question, answer = game.get_game_data()
         print(f'Question: {question}')
         user_answer = prompt.string("Your answer: ")
@@ -30,4 +26,4 @@ def launch_game_round(game):
                   f'Correct answer was {answer}.')
             print(f'Let\'s try again, {username}!')
             return
-    congrats_win(username)
+    print(f'Congratulations, {username}!')
--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -13,13 +13,11 @@ def get_game_data():
     start = randint(MIN_NUM_FOR_START, MAX_NUM_FOR_START)
     end = randint(MIX_NUM_FOR_END, MAX_NUM_FOR_END)
     step = randint(MIN_NUM_FOR_STEP, MAX_NUM_FOR_STEP)
-    progression = range(start, end + start, step)
     progression_list = []
-    for i in progression:
+    for i in range(start, end + start, step):
         progression_list.append(i)
-    difference_set = set(progression_list)
-    progression_list[randint(0, len(progression_list) - 1)] = '..'
-    difference_set = list(difference_set - set(progression_list))
-    answer = difference_set[0]
-    question = ' '.join([str(_) for _ in progression_list])
+    random_index = randint(0, len(progression_list) - 1)
+    answer = progression_list[random_index]
+    progression_list[random_index] = '..'
+    question = ' '.join([str(x) for x in progression_list])
     return question, str(answer)
